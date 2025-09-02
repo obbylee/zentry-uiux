44de114bb37ab705596a27373e3054fe09f625ea
leeobby <leeobbyw@gmail.com>
2025-09-02 21:17:15 +0700

    initial ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// import localFont from "next/font/local";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// Define the local font.
+// const general = localFont({
+//   src: "../public/fonts/general.woff2", // Use the correct file path and name
+//   variable: "--font-general-sans",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
